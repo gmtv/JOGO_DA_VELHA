9440a79699aff5a051314af2567b4c95feebdcd8
gilberto.vieira <gilberto_2_machado@hotmail.com>
2022-03-31 12:16:27 -0300

    Alterado referências de package do projeto

--- src/com/uninter/Jogo.java
+++ src/com/uninter/Jogo.java
@@ -1,46 +1,121 @@
-package com.uninter;
+
+package jogo_da_velha;
+
 
 import java.util.Scanner;
 
 public class Jogo {
+    //Atributos 
+    private Tabuleiro tabuleiro;
+    private int rodada=1, vez=1;
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private int ComputadorA,ComputadorB,ComputadorC;
+    public Scanner entrada = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Tabuleiro tab =new Tabuleiro();
-		Jogador jogador = new Jogador();
-		
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Jogo da velha");
-		System.out.println("Escolha a dificuldade 1,2 ou 3:");
-		int opcao = teclado.nextInt();
-		Computador comp;
-		if(opcao==1) {
-			comp= new ComputadorA();
-		}
-		//definir comportamento para as outras op��es
-		int vez=1;
-		tab.visualizar();
-		
-		while(tab.situacao()==0) {
-			
-			if(vez==1) { //Jogador Humano
-				jogadador.jogar(tab);
-				vez=2;
-			}
-			else if(vez==2) {
-				System.out.println("Jogada Computador");
-				comp.jogar(tab);
-				vez=1;
-			}
-			
-			tab.visualizar();
-			
-		}
-		
-		//Verificar a situacao e imprimir mensagem adequada informando quem venceu ou se empatou
-		
-		
-		
+    //Inicio do jogo
+    public Jogo(){
+        tabuleiro = new Tabuleiro();
+        
+        iniciarJogadores();
+        
+         while( Jogar() );
+        
+    }
+    
+        
+    //Identificação de cada jogador
+    public void iniciarJogadores(){
+        System.out.println("Quem vai ser o Jogador 1 ? Marque 1 para Humano ou 2 Para Computador");
+        if(escolherJogador() == 1)
+            this.jogador1 = new Humano(1);
+        else
+            this.jogador1 = new Computador(1);
+        
+        System.out.println("----------------------");
+        System.out.println("Quem vai ser o Jogador 2 ? Marque 1 para Humano ou 2 Para Computador");
+        
+        if(escolherJogador() == 1)
+            this.jogador2 = new Humano(2);
+        else
+            this.jogador2 = new Computador(2);
+        
+    }
+    
+    public int escolherJogador(){
+        int opcao=0;
+        
+        do{
+            System.out.println("1. Humano");
+            System.out.println("2. Computador\n");
+            System.out.print("Opção: ");
+            opcao = entrada.nextInt();
+            
+            if(opcao != 1 && opcao != 2)
+                System.out.println("Opção inválida! Tente novamente");
+        }while(opcao != 1 && opcao != 2);
+        
+        return opcao;
+    }
+    
+    //Controle do jogo
+    public boolean Jogar(){
+        if(ganhou() == 0 ){
+            System.out.println("----------------------");
+            System.out.println("\nRodada "+rodada);
+            System.out.println("É a vez do jogador " + vez() );
+            
+            if(vez()==1)
+                jogador1.jogar(tabuleiro);
+            else
+                jogador2.jogar(tabuleiro);
+            
+            
+            if(tabuleiro.tabuleiroCompleto()){
+                System.out.println("Tabuleiro Completo. Jogo empatado");
+                return false;
+            }
+            vez++;
+            rodada++;
 
-	}
-
+            return true;
+        } else{
+            if(ganhou() == -1 )
+                System.out.println("Jogador 1 ganhou!");
+            else
+                System.out.println("Jogador 2 ganhou!");
+            
+            return false;
+        }
+            
+    }
+    
+    
+    public int vez(){
+        if(vez%2 == 1)
+            return 1;
+        else
+            return 2;
+    }
+    
+    public int ganhou(){
+        if(tabuleiro.checkLinhas() == 1)
+            return 1;
+        if(tabuleiro.checkColunas() == 1)
+            return 1;
+        if(tabuleiro.checkDiagonais() == 1)
+            return 1;
+        
+        if(tabuleiro.checkLinhas() == -1)
+            return -1;
+        if(tabuleiro.checkColunas() == -1)
+            return -1;
+        if(tabuleiro.checkDiagonais() == -1)
+            return -1;
+        
+        return 0;
+    }
+    
+    
 }
+
--- src/com/uninter/Tabuleiro.java
+++ src/com/uninter/Tabuleiro.java
@@ -1,34 +1,110 @@
-package com.uninter;
+
+package jogo_da_velha;
+
 
 public class Tabuleiro {
-	public int mat[][] = new int[3][3];
-	//1 - X - Jogador humano
-	//-1 - O - Computador
-	//0 - Espa�o vazio
-	public int situacao() {
-		//criar logica de verificar quem venceu ou se empatou ou se o jogo continua
-		return 0;
-	}
-	
-	public void visualizar() {
-		
-		for (int i = 0; i < mat.length; i++) {//linhas
-			for (int j = 0; j < mat.length; j++) {//colunas
-				if(mat[i][j]==1) {
-					System.out.print("X ");
-				}
-				else if(mat[i][j]==-1) {
-					System.out.print("O ");
-				}
-				else {
-					System.out.print("- ");
-				}
-			}
-			System.out.println();
-		}
-		
-	}
-	
-	
+    //Criando a Matriz do jogo
+    private int[][] tabuleiro= new int[3][3];
+    
+    //Recebe o valor do inicio igual a zero
+    public Tabuleiro(){
+        zerarTabuleiro();
+    }
+    
+    public void zerarTabuleiro(){
+        for(int linha=0 ; linha<3 ; linha++)
+            for(int coluna=0 ; coluna<3 ; coluna++)
+                tabuleiro[linha][coluna]=0;
+    }
+    
+    //Exibe a Matriz na tela
+    public void exibeTabuleiro(){
+        System.out.println();
+        for(int linha=0 ; linha<3 ; linha++){
+        
+            for(int coluna=0 ; coluna<3 ; coluna++){
+                
+                if(tabuleiro[linha][coluna]==-1){
+                    System.out.print(" X ");
+                }
+                if(tabuleiro[linha][coluna]==1){
+                    System.out.print(" O ");
+                }
+                                
+                if(tabuleiro[linha][coluna]==0){
+                    System.out.print("   ");
+                }
+                
+                if(coluna==0 || coluna==1)
+                    System.out.print("|");
+            }
+            System.out.println();
+        }
+                
+    }
 
+    
+    public int getPosicao(int[] tentativa){
+        //Retorno, tentativa[0] é a coluna e a tentativa[2] é a coluna
+        return tabuleiro[tentativa[0]][tentativa[1]];
+    }
+    
+    public void setPosicao(int[] tentativa, int jogador){
+        if(jogador == 1)
+            tabuleiro[tentativa[0]][tentativa[1]] = -1;
+        else
+            tabuleiro[tentativa[0]][tentativa[1]] = 1;
+        
+        exibeTabuleiro();
+    }
+
+    //Validam todas as linha e colunas
+    public int checkLinhas(){
+        for(int linha=0 ; linha<3 ; linha++){
+
+            if( (tabuleiro[linha][0] + tabuleiro[linha][1] + tabuleiro[linha][2]) == -3)
+                return -1;
+            if( (tabuleiro[linha][0] + tabuleiro[linha][1] + tabuleiro[linha][2]) == 3)
+                return 1;
+        }
+        
+        return 0;
+                
+    }
+    
+    public int checkColunas(){
+        for(int coluna=0 ; coluna<3 ; coluna++){
+
+            if( (tabuleiro[0][coluna] + tabuleiro[1][coluna] + tabuleiro[2][coluna]) == -3)
+                return -1;
+            if( (tabuleiro[0][coluna] + tabuleiro[1][coluna] + tabuleiro[2][coluna]) == 3)
+                return 1;
+        }
+        
+        return 0;
+                
+    }
+    
+    public int checkDiagonais(){
+        if( (tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2]) == -3)
+            return -1;
+        if( (tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2]) == 3)
+            return 1;
+        if( (tabuleiro[0][2] + tabuleiro[1][1] + tabuleiro[2][0]) == -3)
+            return -1;
+        if( (tabuleiro[0][2] + tabuleiro[1][1] + tabuleiro[2][0]) == 3)
+            return 1;
+        
+        return 0;
+    }
+
+    //Retorno da matriz se esta completa ou não
+    public boolean tabuleiroCompleto(){
+        for(int linha=0 ; linha<3 ; linha++)
+            for(int coluna=0 ; coluna<3 ; coluna++)
+                if( tabuleiro[linha][coluna]==0 )
+                    return false;
+        return true;
+    }
 }
+
